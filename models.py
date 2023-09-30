146b3942174837f58cdb4dfc141ab4cfeb4c0856
T6nisValk <131583739+T6nisValk@users.noreply.github.com>
2023-09-30 13:16:10 +0300

    Created one / many to many associations

--- models.py
+++ models.py
@@ -1,14 +1,40 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy import create_engine, Table, Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 engine = create_engine("sqlite:///metflix.db")
 Base = declarative_base()
 
 
+user_series = Table(
+    "user_series",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("series_id", Integer, ForeignKey("series.id")),
+)
+user_movies = Table(
+    "user_movies",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("movie_id", Integer, ForeignKey("movies.id")),
+)
+watch_list_series = Table(
+    "watch_list_series",
+    Base.metadata,
+    Column("watch_list_id", Integer, ForeignKey("watch_list.id")),
+    Column("series_id", Integer, ForeignKey("series.id")),
+)
+watch_list_movies = Table(
+    "watch_list_movies",
+    Base.metadata,
+    Column("watch_list_id", Integer, ForeignKey("watch_list.id")),
+    Column("movie_id", Integer, ForeignKey("movies.id")),
+)
+
+
 class Movie(Base):
     __tablename__ = "movies"
 
-    movie_id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     movie_title = Column(String(255), nullable=False)
     release_year = Column(Integer, nullable=False)
     rating = Column(Integer, nullable=False)
@@ -17,7 +43,7 @@ class Movie(Base):
 class Series(Base):
     __tablename__ = "series"
 
-    series_id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     series_title = Column(String(255), nullable=False)
     release_year = Column(Integer, nullable=False)
     rating = Column(Integer, nullable=False)
@@ -27,18 +53,24 @@ class Series(Base):
 class User(Base):
     __tablename__ = "users"
 
-    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
     surname = Column(String(255), default="")
+
+    # Can access watch lists and if deleted, deletes all the watch lists for this user - One To Many
+    watch_lists = relationship("WatchList", back_populates="user", cascade="all, delete-orphan")
 
 
 class WatchList(Base):
     __tablename__ = "watch_list"
 
-    watch_list_id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     watch_list_name = Column(String(255), nullable=False)
-    watch_list_creation_date = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey("users.user_id"))
+    watch_list_creation_date = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    # Can access user - One To Many
+    user = relationship("User", back_populates="watch_lists")
 
 
 if __name__ == "__main__":
